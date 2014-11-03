51e5e2f83b7dc4871c2a3e315432c5eaa1bd4bf9
hyunghunny <hyunghunny@gmail.com>
2014-11-03 23:30:14 +0900

    Revert "add favicon.js"
    This reverts commit 679d4765b33c81e0f2947181ef8c9a727865bc74.

--- DIoTY/app.js
+++ DIoTY/app.js
@@ -38,7 +38,8 @@ app.use(allowCrossDomain);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));

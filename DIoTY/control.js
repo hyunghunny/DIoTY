4c7633feb5e269219ad222324b5dd9c0601825f7
Hyunghun <webofthink@snu.ac.kr>
2015-08-18 23:19:57 +0900

    log message typo error fix

--- DIoTY/control.js
+++ DIoTY/control.js
@@ -30,20 +30,20 @@ function transmit(temperature, humidity) {
     if (isLogin) {
         var sender = transmitter;
         sender.emit(1, tempObs, function (result) {
-            console.log('temps transmitted: ' + result);
+            console.log('temperature transmitted: ' + result);
         });
         sender.emit(2, humObs, function (result) {
-            console.log('humiditys transmitted: ' + result);
+            console.log('humidity transmitted: ' + result);
         });
     } else {
         transmitter.login(id, password, function (sender) {
             if (sender) {
                 isLogin = true;
                 sender.emit(1, tempObs, function (result) {
-                    console.log('temps transmitted: ' + result);
+                    console.log('temperature transmitted: ' + result);
                 });
                 sender.emit(2, humObs, function (result) {
-                    console.log('humiditys transmitted: ' + result);
+                    console.log('humidity transmitted: ' + result);
                 });
             } else {
                 console.log('login error!');

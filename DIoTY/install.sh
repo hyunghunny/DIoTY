c0ff9bdd47f85592afa4c78292a99a7aca6ffc8b
Hyunghun <webofthink@snu.ac.kr>
2015-08-21 18:03:42 +0900

    add startup install script

---
+++ DIoTY/install.sh
@@ -0,0 +1,11 @@
+﻿#! /bin/sh
+SCRIPT_NAME=dioty
+
+
+# copy script file as executable into init.d
+sudo cp start.sh /etc/init.d/$SCRIPT_NAME
+sudo chmod 755 /etc/init.d/$SCRIPT_NAME
+
+# register script to be run at start-up
+sudo update-rc.d $SCRIPT_NAME defaults
+
